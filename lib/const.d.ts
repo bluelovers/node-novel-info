89217d68b7a4f65a6989513d0a05ba9dcd200e56
bluelovers <codelovers@users.sourceforge.net>
2018-12-23 18:01:47 +0800

    add desc

--- lib/const.d.ts
+++ lib/const.d.ts
@@ -1,10 +1,25 @@
 /**
  * Created by user on 2018/12/22/022.
  */
+/**
+ * 小說狀態 flag 根據 readme.md 內設定
+ */
 export declare enum EnumNovelStatus {
     UNKNOW = 0,
+    /**
+     * 作者已完結
+     */
     AUTHOR_DONE = 1,
+    /**
+     * 作者已棄坑
+     */
     AUTHOR_NOUPDATE = 2,
+    /**
+     * 作者已刪除
+     */
     AUTHOR_DELETE = 4,
+    /**
+     * 本書已完結 並且處理完畢
+     */
     USER_DONE = 16
 }
--- lib/const.ts
+++ lib/const.ts
@@ -2,13 +2,28 @@
  * Created by user on 2018/12/22/022.
  */
 
+/**
+ * 小說狀態 flag 根據 readme.md 內設定
+ */
 export enum EnumNovelStatus
 {
 	UNKNOW = 0x0000,
 
+	/**
+	 * 作者已完結
+	 */
 	AUTHOR_DONE = 0x0001,
+	/**
+	 * 作者已棄坑
+	 */
 	AUTHOR_NOUPDATE = 0x0002,
+	/**
+	 * 作者已刪除
+	 */
 	AUTHOR_DELETE = 0x00004,
 
+	/**
+	 * 本書已完結 並且處理完畢
+	 */
 	USER_DONE = 0x0010,
 }
